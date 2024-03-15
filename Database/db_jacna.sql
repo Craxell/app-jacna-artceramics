4ef2d6c01ed359a835a6836cef0c564a30d7f9a7
Craxell <andresaltamirasanz@gmail.com>
2024-03-15 15:13:51 -0500

    Update 15-03-2024 - v2

---
+++ Database/db_jacna.sql
@@ -0,0 +1,504 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3307
+-- Tiempo de generación: 15-03-2024 a las 21:11:47
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_jacna`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `idcategoria` bigint(20) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `descripcion` text NOT NULL,
+  `portada` varchar(100) NOT NULL,
+  `datecreated` datetime NOT NULL DEFAULT current_timestamp(),
+  `ruta` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`idcategoria`, `nombre`, `descripcion`, `portada`, `datecreated`, `ruta`, `status`) VALUES
+(9, 'Ceramicas', 'Ceramicas sin planta', 'img_71fe35296118fe128a87035557a66f8a.jpg', '2024-03-15 14:48:03', '', 1),
+(10, 'Ceramicas con planta', 'Las mejores ceramicas', 'img_1f67e3dc4e8c01669350e6a8fd44d75e.jpg', '2024-03-15 14:48:45', '', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_pedido`
+--
+
+CREATE TABLE `detalle_pedido` (
+  `id` bigint(20) NOT NULL,
+  `pedidoid` bigint(20) NOT NULL,
+  `productoid` bigint(20) NOT NULL,
+  `precio` decimal(11,2) NOT NULL,
+  `cantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_temp`
+--
+
+CREATE TABLE `detalle_temp` (
+  `id` bigint(20) NOT NULL,
+  `personaid` bigint(20) NOT NULL,
+  `productoid` bigint(20) NOT NULL,
+  `precio` decimal(11,2) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `transaccionid` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `imagen`
+--
+
+CREATE TABLE `imagen` (
+  `id` bigint(20) NOT NULL,
+  `productoid` bigint(20) NOT NULL,
+  `img` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `imagen`
+--
+
+INSERT INTO `imagen` (`id`, `productoid`, `img`) VALUES
+(37, 14, 'pro_b73647c4898c63f5e204302153e64a30.jpg'),
+(38, 15, 'pro_e4b3729340499cafbb664faec6746811.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `modulo`
+--
+
+CREATE TABLE `modulo` (
+  `idmodulo` bigint(20) NOT NULL,
+  `titulo` varchar(50) NOT NULL,
+  `descripcion` text NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `modulo`
+--
+
+INSERT INTO `modulo` (`idmodulo`, `titulo`, `descripcion`, `status`) VALUES
+(1, 'Dashboard', 'Dashboard', 1),
+(2, 'Usuarios', 'Usuarios del sistema', 1),
+(3, 'Clientes', 'Clientes de tienda', 1),
+(4, 'Productos', 'Todos los productos', 1),
+(5, 'Pedidos', 'Pedidos', 1),
+(6, 'Caterogías', 'Caterogías Productos', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `idpedido` bigint(20) NOT NULL,
+  `personaid` bigint(20) NOT NULL,
+  `fecha` datetime NOT NULL DEFAULT current_timestamp(),
+  `monto` decimal(11,2) NOT NULL,
+  `tipopagoid` bigint(20) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permisos`
+--
+
+CREATE TABLE `permisos` (
+  `idpermiso` bigint(20) NOT NULL,
+  `rolid` bigint(20) NOT NULL,
+  `moduloid` bigint(20) NOT NULL,
+  `r` int(11) NOT NULL DEFAULT 0,
+  `w` int(11) NOT NULL DEFAULT 0,
+  `u` int(11) NOT NULL DEFAULT 0,
+  `d` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `permisos`
+--
+
+INSERT INTO `permisos` (`idpermiso`, `rolid`, `moduloid`, `r`, `w`, `u`, `d`) VALUES
+(350, 2, 1, 1, 0, 0, 0),
+(351, 2, 2, 0, 0, 0, 0),
+(352, 2, 3, 0, 0, 0, 0),
+(353, 2, 4, 0, 0, 0, 0),
+(354, 2, 5, 0, 0, 0, 0),
+(355, 2, 6, 0, 0, 0, 0),
+(596, 3, 1, 1, 0, 0, 0),
+(597, 3, 2, 0, 0, 0, 0),
+(598, 3, 3, 0, 0, 0, 0),
+(599, 3, 4, 0, 0, 0, 0),
+(600, 3, 5, 0, 0, 0, 0),
+(601, 3, 6, 0, 0, 0, 0),
+(608, 1, 1, 1, 1, 1, 1),
+(609, 1, 2, 1, 1, 1, 1),
+(610, 1, 3, 1, 1, 1, 1),
+(611, 1, 4, 1, 1, 1, 1),
+(612, 1, 5, 1, 1, 1, 1),
+(613, 1, 6, 1, 1, 1, 1),
+(614, 11, 1, 1, 0, 0, 0),
+(615, 11, 2, 0, 0, 0, 0),
+(616, 11, 3, 1, 0, 0, 0),
+(617, 11, 4, 1, 1, 1, 1),
+(618, 11, 5, 1, 1, 1, 1),
+(619, 11, 6, 1, 1, 1, 1),
+(620, 7, 1, 0, 0, 0, 0),
+(621, 7, 2, 0, 0, 0, 0),
+(622, 7, 3, 0, 0, 0, 0),
+(623, 7, 4, 0, 0, 0, 0),
+(624, 7, 5, 0, 0, 0, 0),
+(625, 7, 6, 0, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `persona`
+--
+
+CREATE TABLE `persona` (
+  `idpersona` bigint(20) NOT NULL,
+  `identificacion` varchar(30) DEFAULT NULL,
+  `nombres` varchar(80) NOT NULL,
+  `apellidos` varchar(100) NOT NULL,
+  `telefono` bigint(20) NOT NULL,
+  `email_user` varchar(100) NOT NULL,
+  `password` varchar(75) NOT NULL,
+  `nit` varchar(20) DEFAULT NULL,
+  `nombrefiscal` varchar(80) DEFAULT NULL,
+  `direccionfiscal` varchar(100) DEFAULT NULL,
+  `token` varchar(100) DEFAULT NULL,
+  `rolid` bigint(20) NOT NULL,
+  `datecreated` datetime NOT NULL DEFAULT current_timestamp(),
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`idpersona`, `identificacion`, `nombres`, `apellidos`, `telefono`, `email_user`, `password`, `nit`, `nombrefiscal`, `direccionfiscal`, `token`, `rolid`, `datecreated`, `status`) VALUES
+(1, '1.110.285.665', 'Carlos', 'Altamirano', 3195206408, 'carlos.altamirano@uao.edu.co', 'bf33d9267695135419718fac7a22f25a2006500cf629a46b5bef01b20a1ce473', '12341564', 'Don Carlos', 'Algún lugar de Yumbo', 'b49269b4ca3e583a169c-d3e0db6c7eddff281293-b1ac664a0e5ffa32cf0d-4b155845661a3d6feafd', 1, '2020-08-13 00:51:44', 1),
+(25, NULL, 'Sdsa', 'Asda', 2344, 'cliente1@cliente.com', 'c4393250217a02c5900a52bf2da6ceaf20ff37939a2b2482ad92fb163ea0e6d0', NULL, NULL, NULL, NULL, 7, '2024-03-15 14:57:19', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `idproducto` bigint(20) NOT NULL,
+  `categoriaid` bigint(20) NOT NULL,
+  `codigo` varchar(30) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `descripcion` text NOT NULL,
+  `precio` decimal(11,2) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `imagen` varchar(100) NOT NULL,
+  `datecreated` datetime NOT NULL DEFAULT current_timestamp(),
+  `ruta` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`idproducto`, `categoriaid`, `codigo`, `nombre`, `descripcion`, `precio`, `stock`, `imagen`, `datecreated`, `ruta`, `status`) VALUES
+(14, 9, '1500000', 'Carlos', '<p>Hermoso programador &lt;3</p>', 9500000.00, 1, '', '2024-03-15 14:50:18', '', 1),
+(15, 10, '98516', 'Juan_eduardo', '<p>solo juan eduardo</p>', 2500.00, 1, '', '2024-03-15 14:50:46', '', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `idrol` bigint(20) NOT NULL,
+  `nombrerol` varchar(50) NOT NULL,
+  `descripcion` text NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`idrol`, `nombrerol`, `descripcion`, `status`) VALUES
+(1, 'Administrador', 'Acceso a todo el sistema', 1),
+(2, 'Supervisores', 'Supervisor de tienda', 0),
+(3, 'Vendedores', 'Acceso a módulo ventas', 0),
+(4, 'Servicio al cliente', 'Servicio al cliente sistema', 0),
+(5, 'Bodega', 'Bodega', 0),
+(6, 'Resporteria', 'Resporteria Sistema', 0),
+(7, 'Cliente', 'Clientes tienda', 1),
+(8, 'Ejemplo rol', 'Ejemplo rol sitema', 0),
+(9, 'Coordinador', 'Coordinador', 0),
+(10, 'Consulta Ventas', 'Consulta Ventas', 0),
+(11, 'Directivo ArtCeramics', 'Directivo ArtCeramics', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipopago`
+--
+
+CREATE TABLE `tipopago` (
+  `idtipopago` bigint(20) NOT NULL,
+  `tipopago` varchar(100) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `tipopago`
+--
+
+INSERT INTO `tipopago` (`idtipopago`, `tipopago`, `status`) VALUES
+(1, 'PayPal', 1),
+(2, 'Efectivo', 1),
+(3, 'Tarjeta', 1),
+(4, 'Cheque', 1),
+(5, 'Despósito Bancario', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`idcategoria`);
+
+--
+-- Indices de la tabla `detalle_pedido`
+--
+ALTER TABLE `detalle_pedido`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pedidoid` (`pedidoid`),
+  ADD KEY `productoid` (`productoid`);
+
+--
+-- Indices de la tabla `detalle_temp`
+--
+ALTER TABLE `detalle_temp`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `productoid` (`productoid`),
+  ADD KEY `personaid` (`personaid`);
+
+--
+-- Indices de la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `productoid` (`productoid`);
+
+--
+-- Indices de la tabla `modulo`
+--
+ALTER TABLE `modulo`
+  ADD PRIMARY KEY (`idmodulo`);
+
+--
+-- Indices de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`idpedido`),
+  ADD KEY `personaid` (`personaid`),
+  ADD KEY `tipopagoid` (`tipopagoid`);
+
+--
+-- Indices de la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  ADD PRIMARY KEY (`idpermiso`),
+  ADD KEY `rolid` (`rolid`),
+  ADD KEY `moduloid` (`moduloid`);
+
+--
+-- Indices de la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`idpersona`),
+  ADD KEY `rolid` (`rolid`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`idproducto`),
+  ADD KEY `categoriaid` (`categoriaid`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`idrol`);
+
+--
+-- Indices de la tabla `tipopago`
+--
+ALTER TABLE `tipopago`
+  ADD PRIMARY KEY (`idtipopago`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `idcategoria` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_pedido`
+--
+ALTER TABLE `detalle_pedido`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_temp`
+--
+ALTER TABLE `detalle_temp`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT de la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT de la tabla `modulo`
+--
+ALTER TABLE `modulo`
+  MODIFY `idmodulo` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `idpedido` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  MODIFY `idpermiso` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=626;
+
+--
+-- AUTO_INCREMENT de la tabla `persona`
+--
+ALTER TABLE `persona`
+  MODIFY `idpersona` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `idproducto` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `idrol` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `tipopago`
+--
+ALTER TABLE `tipopago`
+  MODIFY `idtipopago` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalle_pedido`
+--
+ALTER TABLE `detalle_pedido`
+  ADD CONSTRAINT `detalle_pedido_ibfk_1` FOREIGN KEY (`pedidoid`) REFERENCES `pedido` (`idpedido`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detalle_pedido_ibfk_2` FOREIGN KEY (`productoid`) REFERENCES `producto` (`idproducto`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `detalle_temp`
+--
+ALTER TABLE `detalle_temp`
+  ADD CONSTRAINT `detalle_temp_ibfk_1` FOREIGN KEY (`productoid`) REFERENCES `producto` (`idproducto`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  ADD CONSTRAINT `imagen_ibfk_1` FOREIGN KEY (`productoid`) REFERENCES `producto` (`idproducto`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`personaid`) REFERENCES `persona` (`idpersona`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pedido_ibfk_2` FOREIGN KEY (`tipopagoid`) REFERENCES `tipopago` (`idtipopago`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  ADD CONSTRAINT `permisos_ibfk_1` FOREIGN KEY (`rolid`) REFERENCES `rol` (`idrol`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `permisos_ibfk_2` FOREIGN KEY (`moduloid`) REFERENCES `modulo` (`idmodulo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD CONSTRAINT `persona_ibfk_1` FOREIGN KEY (`rolid`) REFERENCES `rol` (`idrol`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`categoriaid`) REFERENCES `categoria` (`idcategoria`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
